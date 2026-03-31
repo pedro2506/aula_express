fa3f0bbe436b2c199b4626b5a4fd0d95961d0ff8
pedro2506 <59938479+pedro2506@users.noreply.github.com>
2026-03-31 13:35:25 +0000

    começo

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Requisição recebida!")
+});
+
+app.get('/home', (req, res) => {
+    res.send("Home Page");
+});
+
+app.get('/me', (req, res) => {
+    const {nome, cidade} = req.query;
+    res.send(`Meu nome é ${nome}, Moro em ${cidade}`);
+});
+
+app.get('/ME/:nome/:cidade', (req, res) => { 
+    const {nome, cidade} = req.params;
+    res.send(`Meu nome é ${nome}, moro em ${cidade}`);
+})
+
+app.get('/ME', (req, res) => { --colcar /me/pedro/fortaleza
+    const {nome, cidade} = req.query;
+    res.send(`Meu nome é ${nome}, moro em ${cidade}`);
+})
+
+
+app.listen(1234, () => {
+    console.log("Servidor iniciado na porta 1234");
+});
