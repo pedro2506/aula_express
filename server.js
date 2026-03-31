a64d47380ce7b8903491bc6380a7743a35683194
pedro2506 <59938479+pedro2506@users.noreply.github.com>
2026-03-31 14:37:03 +0000

    up11

--- server.js
+++ server.js
@@ -30,12 +30,13 @@ app.get('/ME/:nome/:cidade', (req, res) => {
     const { nome, cidade } = req.params;
     res.send(`<body style="background: #121212; color: white; font-family: sans-serif; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
             <div style="text-align: center; border: 2px solid #00ff88; padding: 40px; border-radius: 15px; box-shadow: 0 0 20px rgba(0,255,136,0.2);">
-                <h1 style="color: #00ff88; margin-bottom: 10px;">Perfil do Usuário</h1>
+                <h1 style="color: #00ff88; margin-bottom: 10px;">Aula Express</h1>
                 <p style="font-size: 1.5rem;">Olá, meu nome é <strong>${nome}</strong></p>
-                <p style="color: #aaa;">Moro em <strong>${city}</strong></p>
+                <p style="font-size: 1.5rem; style="color: #00ff88;">Moro em <strong>${cidade}</strong></p>
             </div>
         </body>
     `);
+    });
 
 // O listen mantém o processo do Node vivo
 app.listen(3000, () => {
