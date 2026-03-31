d871bbfc891a549c271d71bf9b8fea197d158f15
pedro2506 <59938479+pedro2506@users.noreply.github.com>
2026-03-31 14:58:33 +0000

    up12

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 /*app.use(express.static(path.join(__dirname, 'publico')));
 app.get ('/', (req, res) => {
     res.send('Requisição recebida!');
@@ -39,6 +40,6 @@ app.get('/ME/:nome/:cidade', (req, res) => {
     });
 
 // O listen mantém o processo do Node vivo
-app.listen(3000, () => {
-    console.log("Servidor iniciado na porta 3000");
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
