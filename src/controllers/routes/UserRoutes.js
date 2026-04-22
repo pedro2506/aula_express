22024e2e0fc42559c8361fe999983e24932f06ac
pedro2506 <t3pedropaulo@gmail.com>
2026-04-22 12:32:21 -0300

    up17

--- src/controllers/routes/UserRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { ListUser, CreateUser } = require("../controllers/User");
-
-const route = Router();
-
-route.get("/users", ListUser);
-route.post("/users", CreateUser);
-
-module.exports = route;
